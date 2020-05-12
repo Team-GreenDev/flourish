665645506f8356fc9279c13816aff2d7241425db
James Easter <james.m.easter@gmail.com>
2020-05-12 15:12:52 -0500

    (cleanup) rearrange front end for clarity

--- client /store/reducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import plantsReducer from './plants';
-
-// Future reducers can be imported above like plantsReducer
-// then added as a key value to the combineReducers
-// combineReducers is then passed into the creation of the store in configureStore
-export default combineReducers({
-  plants: plantsReducer,
-});
