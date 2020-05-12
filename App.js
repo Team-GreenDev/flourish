d4c224edddb9892599b14c060dfc9f0a595f12d6
James Easter <james.m.easter@gmail.com>
2020-05-12 17:01:46 -0500

    (feat) add the store to the app.js

--- App.js
+++ App.js
@@ -2,22 +2,26 @@ import * as React from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { Provider } from 'react-redux';
+import configureStore from './client/store/configureStore';
 
 import BottomTabNavigator from './client/components/navigation/BottomTabNavigator';
 
 const Stack = createStackNavigator();
+const store = configureStore();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-
-      {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-      <NavigationContainer >
-        <Stack.Navigator>
-          <Stack.Screen name="Root" component={BottomTabNavigator} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+        <NavigationContainer >
+          <Stack.Navigator>
+            <Stack.Screen name="Root" component={BottomTabNavigator} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </View>
+    </Provider>
   );
 }
 
