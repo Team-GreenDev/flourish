9fd18ee6ce93103d2d56ed17e2c6580b8b361350
nicopaulino <nicopaulino8@gmail.com>
2020-05-13 15:28:58 -0500

    (feat) Add Dummy Data to NotificationScreen

--- client/components/screens/NotificationScreen.js
+++ client/components/screens/NotificationScreen.js
@@ -1,18 +1,128 @@
 import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image, TouchableOpacity } from 'react-native';
 
 export default function NotificationScreen() {
   return (
-    <View>
-      <Text style={styles.Text}>This is the Notification Screen :)</Text>
-    </View>
+    <ScrollView>
+      <TouchableOpacity>
+        <View style={styles.notification}>
+          <Text style={styles.notificationText}>MikeBazil liked your Post</Text>
+          <Image style={styles.postImage} source={{uri: 'https://www.mochacasa.com/blog/wp-content/uploads/2015/02/plants-home.jpg'}}/>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <View style={styles.notification}>
+          <Text style={styles.notificationText}>MikeBazil liked your Post</Text>
+          <Image style={styles.postImage} source={{uri: 'https://www.mochacasa.com/blog/wp-content/uploads/2015/02/plants-home.jpg'}}/>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <View style={styles.notification}>
+          <Text style={styles.notificationText}>MikeBazil liked your Post</Text>
+          <Image style={styles.postImage} source={{uri: 'https://www.mochacasa.com/blog/wp-content/uploads/2015/02/plants-home.jpg'}}/>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <View style={styles.notification}>
+          <Text style={styles.notificationText}>MikeBazil liked your Post</Text>
+          <Image style={styles.postImage} source={{uri: 'https://www.mochacasa.com/blog/wp-content/uploads/2015/02/plants-home.jpg'}}/>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <View style={styles.notification}>
+          <Text style={styles.notificationText}>MikeBazil liked your Post</Text>
+          <Image style={styles.postImage} source={{uri: 'https://www.mochacasa.com/blog/wp-content/uploads/2015/02/plants-home.jpg'}}/>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <View style={styles.notification}>
+          <Text style={styles.notificationText}>MikeBazil liked your Post</Text>
+          <Image style={styles.postImage} source={{uri: 'https://www.mochacasa.com/blog/wp-content/uploads/2015/02/plants-home.jpg'}}/>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <View style={styles.notification}>
+          <Text style={styles.notificationText}>MikeBazil liked your Post</Text>
+          <Image style={styles.postImage} source={{uri: 'https://www.mochacasa.com/blog/wp-content/uploads/2015/02/plants-home.jpg'}}/>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <View style={styles.notification}>
+          <Text style={styles.notificationText}>MikeBazil liked your Post</Text>
+          <Image style={styles.postImage} source={{uri: 'https://www.mochacasa.com/blog/wp-content/uploads/2015/02/plants-home.jpg'}}/>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <View style={styles.notification}>
+          <Text style={styles.notificationText}>MikeBazil liked your Post</Text>
+          <Image style={styles.postImage} source={{uri: 'https://www.mochacasa.com/blog/wp-content/uploads/2015/02/plants-home.jpg'}}/>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <View style={styles.notification}>
+          <Text style={styles.notificationText}>MikeBazil liked your Post</Text>
+          <Image style={styles.postImage} source={{uri: 'https://www.mochacasa.com/blog/wp-content/uploads/2015/02/plants-home.jpg'}}/>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <View style={styles.notification}>
+          <Text style={styles.notificationText}>MikeBazil liked your Post</Text>
+          <Image style={styles.postImage} source={{uri: 'https://www.mochacasa.com/blog/wp-content/uploads/2015/02/plants-home.jpg'}}/>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <View style={styles.notification}>
+          <Text style={styles.notificationText}>MikeBazil liked your Post</Text>
+          <Image style={styles.postImage} source={{uri: 'https://www.mochacasa.com/blog/wp-content/uploads/2015/02/plants-home.jpg'}}/>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <View style={styles.notification}>
+          <Text style={styles.notificationText}>MikeBazil liked your Post</Text>
+          <Image style={styles.postImage} source={{uri: 'https://www.mochacasa.com/blog/wp-content/uploads/2015/02/plants-home.jpg'}}/>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <View style={styles.notification}>
+          <Text style={styles.notificationText}>MikeBazil liked your Post</Text>
+          <Image style={styles.postImage} source={{uri: 'https://www.mochacasa.com/blog/wp-content/uploads/2015/02/plants-home.jpg'}}/>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <View style={styles.notification}>
+          <Text style={styles.notificationText}>MikeBazil liked your Post</Text>
+          <Image style={styles.postImage} source={{uri: 'https://www.mochacasa.com/blog/wp-content/uploads/2015/02/plants-home.jpg'}}/>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <View style={styles.notification}>
+          <Text style={styles.notificationText}>MikeBazil liked your Post</Text>
+          <Image style={styles.postImage} source={{uri: 'https://www.mochacasa.com/blog/wp-content/uploads/2015/02/plants-home.jpg'}}/>
+        </View>
+      </TouchableOpacity>
+    </ScrollView>
   
   );
 }
 
 
 const styles = StyleSheet.create({
-  Text: {
-    color: 'darkslateblue'
-  }
+  notification: {
+    flexDirection: 'row',
+    borderWidth: 1, 
+    borderColor: '#000000',
+    backgroundColor: '#e8e6df'
+  },
+  postImage: {
+    height: 75,
+    width: 75,
+    alignSelf: 'center',
+    margin: 5,
+  },
+  notificationText: {
+    fontSize: 20,
+    alignSelf: 'center',
+    marginLeft: 10,
+    marginRight: 100,
+  },
 });
