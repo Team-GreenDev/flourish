428e8e8769f183349de65fa1ed1db0f57726ff68
James Easter <james.m.easter@gmail.com>
2020-05-13 23:54:26 -0500

    (feat) add handlers for /api/messages

---
+++ server/api/messages.js
@@ -0,0 +1,30 @@
+const express = require('@feathersjs/express');
+const db = require('../db');
+
+// Handles all requests to /api/messages
+const messagesRouter = express.Router();
+
+// Gets all messages for user recipient by recipient id
+messagesRouter.get('/received/:id', (req, res) => {
+  db.getRecipientMessages(req, res)
+    .then((messages) => res.status(200).send(messages))
+    .catch((error) => res.status(500).send(error));
+});
+
+// Gets all messages sent by user from user id
+messagesRouter.get('/sent/:id', (req, res) => {
+  db.getSentUserMessages(req, res)
+    .then((messages) => res.status(200).send(messages))
+    .catch((error) => res.status(500).send(error));
+});
+
+// Add message to database => { user_id, recipient_id, text }
+messagesRouter.post('/', (req, res) => {
+  db.addMessage(req, res)
+    .then(() => res.status(201).send('Added a message!'))
+    .catch((error) => res.status(500).send(error));
+});
+
+module.exports = {
+  messagesRouter,
+};
