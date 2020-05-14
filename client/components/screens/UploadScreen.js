a1a7b53c064709dffde8ccaaa08fdf2e43b05e32
nicopaulino <nicopaulino8@gmail.com>
2020-05-14 17:47:17 -0500

    (feat) Add TextInput and Buttons in UploadScreen

--- client/components/screens/UploadScreen.js
+++ client/components/screens/UploadScreen.js
@@ -1,10 +1,27 @@
 import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function UploadScreen() {
   return (
     <View>
-      <Text style={styles.Text}>This is the Upload Screen :)</Text>
+      <TextInput 
+      style={styles.input}
+      placeholder="Post about your plants!"
+      />
+      <View style={styles.iconsView}>
+        <TouchableOpacity>
+          <Ionicons style={styles.icon} name="ios-camera" size={30} />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Ionicons style={styles.icon} name="ios-image" size={30} />
+        </TouchableOpacity>
+      </View>
+      <Button
+      style={styles.button}
+      title="Post"
+      color="forestgreen"
+      />
     </View>
   
   );
@@ -12,7 +29,16 @@ export default function UploadScreen() {
 
 
 const styles = StyleSheet.create({
-  Text: {
-    color: 'darkslateblue'
+  input: {
+    height: 200,
+    backgroundColor: '#ede9d9'
+  },
+  iconsView: {
+    flexDirection: 'row',
+    justifyContent: 'center'
+  },
+  icon: {
+    margin: 5,
+    color: 'forestgreen',
   }
 });
