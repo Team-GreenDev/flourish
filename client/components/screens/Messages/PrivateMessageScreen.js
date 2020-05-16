657ad7750a0dde8817174546ae5d67ea1cc61cfd
nicopaulino <nicopaulino8@gmail.com>
2020-05-16 14:01:29 -0500

    (feat) Add ids to MessageDummt

--- client/components/screens/Messages/PrivateMessageScreen.js
+++ client/components/screens/Messages/PrivateMessageScreen.js
@@ -7,49 +7,57 @@ export default function PrivateMessageScreen({ history }){
     username: 'Chad',
     profilePic: 'https://randomuser.me/api/portraits/men/2.jpg',
     lastMessage: 'Sup you into plants',
-    created_at: '3:20 AM'
+    created_at: '3:20 AM',
+    id: 1,
   },
   {
     username: 'Karen Boomer',
     profilePic: 'https://randomuser.me/api/portraits/women/2.jpg',
     lastMessage: 'Please dont talk 2 me',
-    created_at: '3:27 AM'
+    created_at: '3:27 AM',
+    id: 2,
   },
   {
     username: 'Chad',
     profilePic: 'https://randomuser.me/api/portraits/men/2.jpg',
     lastMessage: 'I love your fern :)',
-    created_at: '8:27 AM'
+    created_at: '8:27 AM',
+    id: 3,
   },
   {
     username: 'Karen Boomer',
     profilePic: 'https://randomuser.me/api/portraits/women/2.jpg',
     lastMessage: 'I have a bf',
-    created_at: '8:47 AM'
+    created_at: '8:47 AM',
+    id: 4,
   },
   {
     username: 'Chad',
     profilePic: 'https://randomuser.me/api/portraits/men/2.jpg',
     lastMessage: 'You are so beautiful',
-    created_at: '11:21 AM'
+    created_at: '11:21 AM',
+    id: 5,
   },
   {
     username: 'Karen Boomer',
     profilePic: 'https://randomuser.me/api/portraits/women/2.jpg',
     lastMessage: 'I will block u',
-    created_at: '11:27 AM'
+    created_at: '11:27 AM',
+    id: 6,
   },
   {
     username: 'Chad',
     profilePic: 'https://randomuser.me/api/portraits/men/2.jpg',
     lastMessage: 'Please anser me',
-    created_at: '12:20 PM'
+    created_at: '12:20 PM',
+    id: 7,
   },
   {
     username: 'Karen Boomer',
     profilePic: 'https://randomuser.me/api/portraits/women/2.jpg',
     lastMessage: 'No!',
-    created_at: '2:27 PM'
+    created_at: '2:27 PM',
+    id: 8,
   },
 ]
 
@@ -59,7 +67,7 @@ export default function PrivateMessageScreen({ history }){
     title="Back"
     onPress={() => history.push("/")}/>
       {messageDummy.map(user => (
-        <TouchableOpacity style={styles.messagesContainer}>
+        <TouchableOpacity style={styles.messagesContainer} key={user.id}>
           <Image style={styles.messagesImage} source={{uri: user.profilePic}}/>
           <View style={styles.vertText} >
             <Text style={styles.messagesUsername}>{user.username}</Text>
