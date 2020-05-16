a517259072d1d6f503378343d995a3dfb69db292
Harley Padua <harleypadua@gmail.com>
2020-05-15 21:27:27 -0500

    feat/ added ability to log in with google, not perfect

--- client/components/LoginScreen.js
+++ client/components/LoginScreen.js
@@ -1,17 +1,33 @@
 import 'react-native-gesture-handler';
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import React from 'react';
+import { View, StyleSheet, Button, Text } from 'react-native';
+import * as Google from 'expo-google-app-auth'
 
+const IOS_CLIENT_ID = '897634645344-qkd9kh2ao7hvdo1c72fnpbg9i6mp4ufi.apps.googleusercontent.com';
+export default function LoginPage () {
 
+  async function signInWithGoogleAsync() {
+    try {
+      const result = await Google.logInAsync({
+        behavior: 'web',
+        iosClientId: IOS_CLIENT_ID,
+        scopes: ['profile', 'email'],
+      });
 
-export default function LoginPage({ login }) {
-  return (
-    <View>
-      <Text style={styles.header}>Sign In With Google</Text>
-      <Button title="Sign in with Google"
-       onPress={() => login.signIn()} />
-    </View>
-  )
+      if (result.type === 'success') {
+        return result.accessToken;
+      } else {
+        return { cancelled: true };
+      }
+    } catch (e) {
+      return { error: true };
+    }
+  }
+    return (
+      <View style={styles.container}>
+        <Button onPress={signInWithGoogleAsync()} title="Sign in with Google" />
+      </View>
+    )
 }
 
 const styles = StyleSheet.create({
