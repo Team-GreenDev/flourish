74b3e41f0116633dd66e8744c527d5a37c495a9a
xoweekday <xoweekday@gmail.com>
2020-05-17 22:49:47 -0500

    (feat) add form for uploading posts

--- client/components/screens/UploadScreen.js
+++ client/components/screens/UploadScreen.js
@@ -1,14 +1,22 @@
-
 import * as React from 'react';
 import { StyleSheet, Text, View, TextInput, Button, TouchableOpacity, Image, SafeAreaView } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import * as ImagePicker from 'expo-image-picker';
-import axios from 'axios';
+import { Formik } from 'formik';
 
 export default function UploadScreen() {
-  const [selectedImage, setSelectedImage] = React.useState("");
+  function AppTextInput({ icon, ...otherProps }) {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          placeholderTextColor="#6e6969"
+          style={styles.text}
+          {...otherProps}
+        />
+      </View>
+    );
+  }
   const [photo, setPhoto] = React.useState('');
-  // 
   const CLOUDINARY_URL = 'https://api.cloudinary.com/v1_1/dsw29lceg/upload'
   const openImagePickerAsync = async () => {
     let permissionResult = await ImagePicker.requestCameraRollPermissionsAsync();
@@ -43,66 +51,97 @@ export default function UploadScreen() {
     }).catch(err => console.log(err))
   };
 
-  const uploadPost = () => {
-    console.log(photo);
-  }
-
-  const removePhoto = () => {
-    setPhoto('');
-  }
   return (
-    <View>
-       <TextInput
-      style={styles.input}
-      placeholder="Post about your plants!"
-      />
-      <SafeAreaView style={styles.imageUploadView}>
-      {photo === '' ? 
-      <Image onPress={() => removePhoto()} style={styles.imageUpload} source={{uri: ''}} /> :
-      <Image style= {styles.imageUpload} source={{uri: photo}} />}
-      </SafeAreaView>
-      <Button
-      onPress={() => console.log('alert')}
-      style={styles.button}
-      title="Post"
-      color="forestgreen"
-      />
-      <View style={styles.iconsView}>
-        {/* <TouchableOpacity>
-          <Ionicons style={styles.icon} name="ios-camera" size={50} />
-        </TouchableOpacity> */}
-        <TouchableOpacity>
-          <Ionicons style={styles.icon} name="ios-image" size={50} onPress={() => openImagePickerAsync()}/>
-        </TouchableOpacity>
-      </View>
-    </View>
+    <Formik
+    initialValues={{description: '', tag: '', image: ''}}
+    onSubmit={values => {
+      values.image = photo;
+      console.log(values)
+    }}
+    >
+      {({handleChange, handleBlur, handleSubmit, values}) => (
+        <>
+          <AppTextInput
+            autoCapitalize="none"
+            autoCorrect={false}
+            onChangeText={handleChange("description")}
+            placeholder="description"
+            textContentType="none"
+          />
+           {/* <AppTextInput
+            maxLength={255}
+            autoCapitalize="none"
+            autoCorrect={true}
+            multiline
+            numberOfLines={3}
+            onChangeText={handleChange("description")}
+            placeholder="Upload Photo"
+            textContentType="none"
+          /> */}
+          <AppTextInput
+            autoCapitalize="none"
+            autoCorrect={true}
+            onChangeText={handleChange("tag")}
+            placeholder="tags"
+            textContentType="none"
+          />
+          <SafeAreaView style={styles.imageUploadView}>
+       {photo === '' ? null : <Image style= {styles.imageUpload} source={{uri: photo}} />}
+       </SafeAreaView>
+          <View style={styles.iconsView}>
+         {/* <TouchableOpacity>
+           <Ionicons style={styles.icon} name="ios-camera" size={50} />
+         </TouchableOpacity> */}
+         <TouchableOpacity>
+           <Ionicons style={styles.icon} name="ios-image" size={50} onPress={() => openImagePickerAsync()}/>
+         </TouchableOpacity>
+       </View>
+          <Button onPress={handleSubmit} title="post" />
+        </>
+  )}
+    </Formik>
   );
 }
 
-
 const styles = StyleSheet.create({
-  input: {
-    height: 150,
-    backgroundColor: '#ede9d9'
-  },
-  iconsView: {
-    flexDirection: 'row',
-    justifyContent: 'center'
+  container: {
+    backgroundColor: "#f8f4f4",
+    borderRadius: 25,
+    flexDirection: "row",
+    width: "100%",
+    padding: 15,
+    marginVertical: 10,
   },
   icon: {
-    margin: 5,
-    color: 'forestgreen',
+  margin: 5,
+  color: 'forestgreen',
   },
-  imageUpload: {
+  text: {
+    color: "#0c0c0c",
+    fontSize: 18,
+    fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
+  },
+   input: {
+   height: 150,
+     backgroundColor: '#ede9d9'
+   },
+   iconsView: {
+   flexDirection: 'row',
+    justifyContent: 'center'
+   },
+   imageUpload: {
     margin: 5,
-    width: 150,
-    height: 150
+    width: 125,
+    height: 125,
+    borderColor: "black",
+    borderWidth: 2
   },
   imageUploadView: {
     flexDirection: 'row',
     justifyContent: 'center'
   }
 });
+
 
 
 
