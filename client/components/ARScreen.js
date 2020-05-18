d07ce5d989ab680a93fe0a749e6da39bc765b05b
chryen <squirtlexomg@yahoo.com>
2020-05-18 18:22:43 -0500

    (feat) allow camera to work using expo three with rerendering for augmented reality

--- client/components/ARScreen.js
+++ client/components/ARScreen.js
@@ -1,50 +1,89 @@
 import React, { useState, useEffect } from 'react';
 import { Text, View, TouchableOpacity } from 'react-native';
-import { Camera } from 'expo-camera';
+import { Asset } from 'expo-asset';
+import { AR } from 'expo';
+import * as Permissions from 'expo-permissions';
+// import { Camera } from 'expo-camera';
+import { loadDaeAsync, Renderer, THREE, utils } from 'expo-three';
+import { GraphicsView } from 'expo-graphics';
+import { BackgroundTexture, Camera, Light } from 'expo-three-ar';
 
 export default function ARScreen() {
-  const [hasPermission, setHasPermission] = useState(null);
-  const [type, setType] = useState(Camera.Constants.Type.back);
+  // const [hasPermission, setHasPermission] = useState(null);
+  // const [type, setType] = useState(Camera.Constants.Type.back);
 
   useEffect(() => {
     (async () => {
-      const { status } = await Camera.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
+      // const { status } = await Camera.requestPermissionsAsync();
+      // setHasPermission(status === 'granted');
+      const { status } = await Permissions.askAsync(Permissions.CAMERA);
+      if (status !== 'granted') {
+        return <Text>No access to camera</Text>;
+      }
+      // Turn off extra warnings
+      THREE.suppressExpoWarnings(true);
+      return <Text>aslkdfsadkjf</Text>;
     })();
   }, []);
 
-  if (hasPermission === null) {
-    return <View />;
+  // if (hasPermission === null) {
+  //   return <GraphicsView />;
+  // }
+  // if (hasPermission === false) {
+  //   return <Text>No access to camera</Text>;
+  // }
+
+  const onContextCreate = async ({ gl, scale: pixelRatio, width, height }) => {
+    // This will allow ARKit to collect Horizontal surfaces
+    AR.setPlaneDetection(AR.PlaneDetectionTypes.Horizontal);
+    renderer = new Renderer({ gl, pixelRatio, width, height });
+    renderer.gammaInput = true;
+    renderer.gammaOutput = true;
+    renderer.shadowMap.enabled = true;
+
+    scene = new THREE.Scene();
+    scene.background = new BackgroundTexture(renderer);
+
+    camera = new Camera(width, height, 0.01, 1000);
+
+    // Make a cube - notice that each unit is 1 meter in real life, we will make our box 0.1 meters
+    const geometry = new THREE.BoxGeometry(0.1, 0.1, 0.1);
+    // Simple color material
+    const material = new THREE.MeshPhongMaterial({
+      color: 0xff00ff,
+    });
   }
-  if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
-  }
+
+    // When the phone rotates, or the view changes size, this method will be called.
+    const onResize = ({ x, y, scale, width, height }) => {
+    // Let's stop the function if we haven't setup our scene yet
+      if (!renderer) {
+        return;
+      }
+      camera.aspect = width / height;
+      camera.updateProjectionMatrix();
+      renderer.setPixelRatio(scale);
+      renderer.setSize(width, height);
+    };
+
+  // Called every frame.
+    const onRender = () => {
+      // This will make the points get more rawDataPoints from Expo.AR
+      // points.update(); idk what this is but commenting out lets camera open so whatever
+      // Finally render the scene with the AR Camera
+      renderer.render(scene, camera);
+    };
+
   return (
-    <View style={{ flex: 1 }}>
-      <Camera style={{ flex: 1 }} type={type}>
-        <View
-          style={{
-            flex: 1,
-            backgroundColor: 'transparent',
-            flexDirection: 'row',
-          }}>
-          <TouchableOpacity
-            style={{
-              flex: 0.1,
-              alignSelf: 'flex-end',
-              alignItems: 'center',
-            }}
-            onPress={() => {
-              setType(
-                type === Camera.Constants.Type.back
-                  ? Camera.Constants.Type.front
-                  : Camera.Constants.Type.back
-              );
-            }}>
-            <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
-          </TouchableOpacity>
-        </View>
-      </Camera>
-    </View>
+    (<GraphicsView
+      style={{ flex: 1 }}
+      onContextCreate={onContextCreate}
+      onRender={onRender}
+      onResize={onResize}
+      isArEnabled
+      isArRunningStateEnabled
+      isArCameraStateEnabled
+      arTrackingConfiguration={'ARWorldTrackingConfiguration'}
+      />)
   );
 }
