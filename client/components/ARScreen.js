81520234b94734896d50588b5d9292ba8ec25788
chryen <squirtlexomg@yahoo.com>
2020-05-18 16:17:44 -0500

    (feat) add basic camera setup

--- client/components/ARScreen.js
+++ client/components/ARScreen.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import ExpoTHREE, { THREE } from 'expo-three';
+import ExpoGraphics from 'expo-graphics';
+
 
 export default class ARScreen extends React.Component {
 
@@ -7,11 +10,31 @@ export default class ARScreen extends React.Component {
     header: null,
   };
 
+  onContextCreate = async ({gl, scale, width, height, arSession}) => {
+    // initialize renderer...
+    this.renderer = ExpoTHREE.createRenderer({gl});
+    this.renderer.setPixelRatio(scale);
+    this.renderer.setSize(width, height);
+
+    // initialize scene...
+    this.scene = new THREE.Scene();
+    this.scene.background = ExpoTHREE.createARBackgroundTexture(arSession, this.renderer);
+
+    // initialize camera...
+    this.camera = ExpoTHREE.createARCamera(arSession, width / scale, height / scale, 0.01, 1000);
+  }
+
+  onRender = (delta) => {
+    this.renderer.render(this.scene, this.camera);
+  }
+
   render() {
     return (
-      <View style={{paddingTop:20}}>
-        <Text>Test</Text>
-      </View>
+      <ExpoGraphics.View style={{flex:1}}
+        onContextCreate={this.onContextCreate}
+        onRender={this.onRender}
+        arEnabled={true}
+      />
     );
   }
   
