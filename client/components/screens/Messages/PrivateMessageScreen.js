f5858d7a3ba9d006f8166f65ecde461bada4e472
nicopaulino <nicopaulino8@gmail.com>
2020-05-18 17:01:24 -0500

    (feat) Change CSS to PrivateMessageScreen

--- client/components/screens/Messages/PrivateMessageScreen.js
+++ client/components/screens/Messages/PrivateMessageScreen.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import {Text, Button, View, StyleSheet, ScrollView, TouchableOpacity, Image, TextInput, SafeAreaView} from 'react-native';
+// var BubbleText = require('react-native-message-bubble');
+import { moderateScale } from 'react-native-size-matters';
+import Svg, {
+  Path,
+} from 'react-native-svg';
 
 export default function PrivateMessageScreen({ history }){
 
@@ -62,60 +67,202 @@ export default function PrivateMessageScreen({ history }){
 ]
 
   return (
-    <ScrollView>
-    <Button
-    title="Back"
-    onPress={() => history.push("/")}/>
-      {messageDummy.map(user => (
-        <TouchableOpacity style={styles.messagesContainer} key={user.id}>
-          <Image style={styles.messagesImage} source={{uri: user.profilePic}}/>
-          <View style={styles.vertText} >
-            <Text style={styles.messagesUsername}>{user.username}</Text>
-            <SafeAreaView>
-              <Text style={styles.messagesText} >{user.lastMessage}</Text>
-            </SafeAreaView>
-          </View>
-          <Text style={styles.timeStamp}>{user.created_at}</Text>
-        </TouchableOpacity>
-      ))}
-    <TextInput 
-    style={styles.input}
-      placeholder="Send a message..."/> 
-    </ScrollView>
+    <View style={[styles.item, styles.itemIn]}>
+    <View style={[styles.balloon, {backgroundColor: 'grey'}]}>
+      <Text style={{paddingTop: 5, color: 'white'}}>Hey! How are you?</Text>
+      <View
+      style={[
+        styles.arrowContainer,
+        styles.arrowLeftContainer,
+      ]}
+    >
+
+       <Svg style={styles.arrowLeft} width={moderateScale(15.5, 0.6)} height={moderateScale(17.5, 0.6)} viewBox="32.484 17.5 15.515 17.5"  enable-background="new 32.485 17.5 15.515 17.5">
+            <Path
+                d="M38.484,17.5c0,8.75,1,13.5-6,17.5C51.484,35,52.484,17.5,38.484,17.5z"
+                fill="grey"
+                x="0"
+                y="0"
+            />
+        </Svg>
+    </View>
+    </View>
+  </View>
+
+
+  // <View style={[styles.item, styles.itemOut]}>
+  //   <View style={[styles.balloon, {backgroundColor: '#1084ff'}]}>
+  //     <Text style={{paddingTop: 5, color: 'white'}}>Hey! I am good. How are you?</Text>
+  //     <View
+  //     style={[
+  //       styles.arrowContainer,
+  //       styles.arrowRightContainer,
+  //     ]}
+  //   >
+  //      <Svg style={styles.arrowRight} width={moderateScale(15.5, 0.6)} height={moderateScale(17.5, 0.6)} viewBox="32.485 17.5 15.515 17.5"  enable-background="new 32.485 17.5 15.515 17.5">
+  //           <Path
+  //               d="M48,35c-7-4-6-8.75-6-17.5C28,17.5,29,35,48,35z"
+  //               fill="#1084ff"
+  //               x="0"
+  //               y="0"
+  //           />
+  //       </Svg>
+  //   </View>
+  //   </View>
+  // </View>
+
+
+
+
+
+  //  <View style={[styles.item, styles.itemOut]}>
+  //   <View style={[styles.balloon, {backgroundColor: '#1084ff'}]}>
+  //     <Text style={{paddingTop: 5, color: 'white'}}>Check this Image out !!!</Text>
+  //     <View
+  //     style={[
+  //       styles.arrowContainer,
+  //       styles.arrowRightContainer,
+  //     ]}
+  //   >
+  //      <Svg style={styles.arrowRight} width={moderateScale(15.5, 0.6)} height={moderateScale(17.5, 0.6)} viewBox="32.485 17.5 15.515 17.5"  enable-background="new 32.485 17.5 15.515 17.5">
+  //           <Path
+  //               d="M48,35c-7-4-6-8.75-6-17.5C28,17.5,29,35,48,35z"
+  //               fill="#1084ff"
+  //               x="0"
+  //               y="0"
+  //           />
+  //       </Svg>
+  //   </View>
+  //   </View>
+  // </View>
+
+
+  //  <View style={[styles.item, styles.itemOut]}>
+  //   <View style={[styles.balloon, {backgroundColor: '#1084ff'}]}>
+
+  //   <Image
+  //       styleName="small"
+  //       borderRadius={5}
+  //       source={{ uri: 'https://shoutem.github.io/img/ui-toolkit/examples/image-3.png'}}
+  //       />
+  //     <View
+  //     style={[
+  //       styles.arrowContainer,
+  //       styles.arrowRightContainer,
+  //     ]}
+  //   >
+  //      <Svg style={styles.arrowRight} width={moderateScale(15.5, 0.6)} height={moderateScale(17.5, 0.6)} viewBox="32.485 17.5 15.515 17.5"  enable-background="new 32.485 17.5 15.515 17.5">
+  //           <Path
+  //               d="M48,35c-7-4-6-8.75-6-17.5C28,17.5,29,35,48,35z"
+  //               fill="#1084ff"
+  //               x="0"
+  //               y="0"
+  //           />
+  //       </Svg>
+  //   </View>
+  //   </View>
+  // </View>
+
+
+//  <View style={[styles.item, styles.itemIn]}>
+//     <View style={[styles.balloon, {backgroundColor: 'grey'}]}>
+//       <Text style={{paddingTop: 5, color: 'white'}}>Nice Picture</Text>
+//       <View
+//       style={[
+//         styles.arrowContainer,
+//         styles.arrowLeftContainer,
+//       ]}
+//     >
+
+//        <Svg style={styles.arrowLeft} width={moderateScale(15.5, 0.6)} height={moderateScale(17.5, 0.6)} viewBox="32.484 17.5 15.515 17.5"  enable-background="new 32.485 17.5 15.515 17.5">
+//             <Path
+//                 d="M38.484,17.5c0,8.75,1,13.5-6,17.5C51.484,35,52.484,17.5,38.484,17.5z"
+//                 fill="grey"
+//                 x="0"
+//                 y="0"
+//             />
+//         </Svg>
+//     </View>
+//     </View>
+//   </View>
   );
 }
 
 const styles = StyleSheet.create({
-  messagesContainer: {
-    flexDirection: 'row',
-  },
-  messagesImage: {
-    height: 50,
-    width: 50,
-    alignSelf: 'center',
-    margin: 7,
-    borderRadius: 100 / 2,
-  },
-  messagesText: {
-    fontSize: 20,
-    marginLeft: 10,
-    marginBottom: 5,
-    // maxWidth: 345,
-  },
-  messagesUsername: {
-    fontSize: 15
-  },
-  vertText: {
-    flexDirection: 'column'
-  },
-  input: {
-    height: 40,
-    fontSize: 20,
-    backgroundColor: '#E9F1E9',
-  },
-  timeStamp: {
-    color: '#94a57e',
-    position: 'absolute',
-    left: 340,
-  }
+  // messagesContainer: {
+  //   flexDirection: 'row',
+  // },
+  // messagesImage: {
+  //   height: 50,
+  //   width: 50,
+  //   alignSelf: 'center',
+  //   margin: 7,
+  //   borderRadius: 100 / 2,
+  // },
+  // messagesText: {
+  //   fontSize: 20,
+  //   marginLeft: 10,
+  //   marginBottom: 5,
+  //   // maxWidth: 345,
+  // },
+  // messagesUsername: {
+  //   fontSize: 15
+  // },
+  // vertText: {
+  //   flexDirection: 'column'
+  // },
+  // input: {
+  //   height: 40,
+  //   fontSize: 20,
+  //   backgroundColor: '#E9F1E9',
+  // },
+  // timeStamp: {
+  //   color: '#94a57e',
+  //   position: 'absolute',
+  //   left: 340,
+  // }
+  item: {
+    marginVertical: moderateScale(7, 2),
+    flexDirection: 'row'
+ },
+ itemIn: {
+     marginLeft: 20
+ },
+ itemOut: {
+    alignSelf: 'flex-end',
+    marginRight: 20
+ },
+ balloon: {
+    maxWidth: moderateScale(250, 2),
+    paddingHorizontal: moderateScale(10, 2),
+    paddingTop: moderateScale(5, 2),
+    paddingBottom: moderateScale(7, 2),
+    borderRadius: 20,
+ },
+ arrowContainer: {
+     position: 'absolute',
+     top: 0,
+     left: 0,
+     right: 0,
+     bottom: 0,
+     zIndex: -1,
+     flex: 1
+ },
+ arrowLeftContainer: {
+     justifyContent: 'flex-end',
+     alignItems: 'flex-start'
+ },
+
+ arrowRightContainer: {
+     justifyContent: 'flex-end',
+     alignItems: 'flex-end',
+ },
+
+ arrowLeft: {
+     left: moderateScale(-6, 0.5),
+ },
+
+ arrowRight: {
+     right:moderateScale(-6, 0.5),
+ }
 });
