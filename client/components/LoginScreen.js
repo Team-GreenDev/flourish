aefd67ba15c4bfb50ce5db5fd5e9d9891727f2da
Harley Padua <harleypadua@gmail.com>
2020-05-19 11:47:39 -0500

    (feat) styled login page and status bar

--- client/components/LoginScreen.js
+++ client/components/LoginScreen.js
@@ -1,6 +1,6 @@
 import 'react-native-gesture-handler';
 import React, { useEffect } from 'react';
-import { View, StyleSheet, Button, Text, Image } from 'react-native';
+import { View, StyleSheet, TouchableOpacity, Text, Image, StatusBar } from 'react-native';
 import * as Google from 'expo-google-app-auth';
 import * as Facebook from 'expo-facebook';
 import { IOS_CLIENT_ID, FACEBOOK_ID, AND_CLIENT_ID } from 'react-native-dotenv';
@@ -80,13 +80,20 @@ export default function LoginScreen ({logInSuccessful}) {
     const signInWithFacebook = () => {
       signInWithFacebookAsync()
     }
-
     return (
       <View style={styles.container}>
-        <View style-={styles.imageContainer}>
-          <Image source={require('../../assets/images/logo.png')} style={styles.logo}/>
+        <StatusBar barStyle = "dark-content" hidden = {false} backgroundColor = "#00BCD4" translucent = {true}/>
+        <View style={styles.backgroundContainer}>
+          <Image style={styles.backgroundImage} source={require('../../assets/images/bbeb8ae14e6ecff8ba823e7e6e6ca6e8.jpg')} />
         </View>
-          <Button onPress={() => signInWithGoogle()} title="Sign in with Google"/>
+        <View style={styles.logo}>
+          <Image source={require('../../assets/images/logo.png')} style={styles.logo}/>
+          </View>
+        <View>
+          <TouchableOpacity onPress={() => signInWithGoogle()}>
+            <Text style={styles.button}>Sign in with Google</Text>
+          </TouchableOpacity>
+          </View>
         {/* <Button onPress={() => signInWithFacebook()} title="Sign in with Facebook" /> */}
       </View>
     )
@@ -97,11 +104,31 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "flex-start",
   },
-  imageContainer: {
-    width: 100,
-    height: 100,
-    marginBottom: -10,
+  backgroundContainer: {
+    flex: 1,
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+}, 
+  backgroundImage: {
+  flex: 1, 
+  width: null, 
+  height: null
+},
+  button: {
+    backgroundColor: "#9C4C33",
+    borderColor: 'white',
+    borderWidth: 1,
+    borderRadius: 12,
+    color: 'white',
+    fontSize: 24,
+    fontWeight: 'bold',
+    overflow: 'hidden',
+    padding: 12,
+    textAlign:'center',
   },
 })
