be5178c5a05dff666d1673fd29526e3ef260e0f2
chryen <squirtlexomg@yahoo.com>
2020-05-19 13:28:00 -0500

    (feat) add light to scene for cube to appear pink

--- client/components/ARScreen.js
+++ client/components/ARScreen.js
@@ -34,6 +34,10 @@ export default function ARScreen() {
 
     camera = new Camera(width, height, 0.01, 1000);
 
+    let light = new THREE.DirectionalLight( 0xffffff );
+    light.position.set( 0, 1, 1 ).normalize();
+    scene.add(light);
+
     // Make a cube - notice that each unit is 1 meter in real life, we will make our box 0.1 meters
     const geometry = new THREE.BoxGeometry(0.1, 0.1, 0.1);
     // Simple color material
