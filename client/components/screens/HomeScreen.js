d31d1f63245a8f847f2aadcb289aa9c858541db6
Harley Padua <harleypadua@gmail.com>
2020-05-20 17:24:12 -0500

    (feat) added like icon to home feed

--- client/components/screens/HomeScreen.js
+++ client/components/screens/HomeScreen.js
@@ -1,18 +1,28 @@
-import React, { useEffect } from 'react';
-import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Image, ScrollView, TouchableOpacity } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import { loadPosts } from '../../store/slices/posts';
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+import Axios from 'axios';
 
-export default function HomeScreen() {
+
+export default function HomeScreen({ likedPosts }) {
   const users = useSelector(state => state.users)
   const posts = useSelector(state => state.posts)
   const dispatch = useDispatch();
+  const [likes, setLikes] = useState({});
 
   const getUserById = (id) => users.list.filter((user) => user.id == id);
 
   useEffect(()=>{
     dispatch(loadPosts());
   }, [posts.postAdded])
+
+  const addLike = (users, posts) => {
+    console.log('liked');
+    // Axios.post(`api/likes/${users.id}/${posts.id}`);
+  }
+
 
   return (
     <ScrollView styles={styles.container}>
@@ -26,7 +36,18 @@ export default function HomeScreen() {
             <Text> </Text>
             <View style={styles.post}>
               <Image style={styles.image} source={{ uri: post.url }}/>
+              <View style={styles.likesContainer}>
               <Text style={styles.username}>{name}</Text>
+              <TouchableOpacity>
+                    <MaterialCommunityIcons
+                    name={"flower-tulip"}
+                    size={24}
+                    raised
+                    style={styles.icon}
+                    onPress={addLike}
+                    />
+              </TouchableOpacity>
+              </View>
               <Text style={styles.message}>{post.text}</Text>
               <Text style={styles.tags}>#favplants #new2flourish</Text>
             </View>
@@ -74,4 +95,12 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     backgroundColor: '#C0CDC6',
   },
+  likesContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  icon: {
+    color: "blue",
+    
+  }
 });
