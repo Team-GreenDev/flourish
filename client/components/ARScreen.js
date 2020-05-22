e7a44116f2b872789b978b0f455af88c100024c5
chryen <squirtlexomg@yahoo.com>
2020-05-22 14:39:44 -0500

    (test) add green banana on screen using

--- client/components/ARScreen.js
+++ client/components/ARScreen.js
@@ -3,9 +3,13 @@ import { Text, View, TouchableOpacity } from 'react-native';
 import { Asset } from 'expo-asset';
 import { AR } from 'expo';
 import * as Permissions from 'expo-permissions';
-import { loadDaeAsync, Renderer, THREE, utils } from 'expo-three';
+import * as THREE from 'three';
+import { loadDaeAsync, Renderer, utils } from 'expo-three';
 import { GraphicsView } from 'expo-graphics';
 import { BackgroundTexture, Camera, Light } from 'expo-three-ar';
+// import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
+import { MTLLoader, OBJLoader } from "three-obj-mtl-loader";
+import * as butts from '../../flower.json';
 
 export default function ARScreen() {
   
@@ -39,17 +43,101 @@ export default function ARScreen() {
     let light = new THREE.DirectionalLight( 0xffffff, 0.5 );
     light.position.set( 3, 3, 3 )
     scene.add(light);
-    
 
+    var addBananaInScene = function(object){
+      banana = object;
+      //Move the banana in the scene
+      banana.rotation.x = Math.PI/2;
+      banana.position.y = -200;
+      banana.position.z = 50;
+      //Go through all children of the loaded object and search for a Mesh
+      object.traverse( function ( child ) {
+          //This allow us to check if the children is an instance of the Mesh constructor
+          if(child instanceof THREE.Mesh){
+              child.material.color = new THREE.Color(0X00FF00);
+              //Sometimes there are some vertex normals missing in the .obj files, ThreeJs will compute them
+              child.geometry.computeVertexNormals();
+          }
+      });
+      //Add the 3D object in the scene
+      scene.add(banana);
+      onRender();
+    };
+
+    var loadOBJ = function(){
+      //Manager from ThreeJs to track a loader and its status
+      var manager = new THREE.LoadingManager();
+      //Loader for Obj from Three.js
+      var loader = new THREE.OBJLoader( manager );
+      //Launch loading of the obj file, addBananaInScene is the callback when it's ready 
+      loader.load( 'http://mamboleoo.be/learnThree/demos/banana.obj', addBananaInScene);
+    };
+    loadOBJ();
+    // --------------------------------------------------
+    // var mtlLoader = new THREE.MTLLoader();
+    // mtlLoader.setPath( '/' );
+    // var url = "indoor plant_02.mtl";
+    // mtlLoader.load( url, function( materials ) {
+    //   console.log('sakdfhadsf')
+    //   materials.preload();
+
+    //   var objLoader = new THREE.OBJLoader();
+    //   objLoader.setMaterials( materials );
+    //   objLoader.setPath( '/' );
+    //   objLoader.load( 'indoor plant_02.obj', function ( object ) {
+
+    //     object.position.y = - 95;
+    //     scene.add( object );
+
+    //   }, onProgress, onError );
+
+    // });
+    // -------------------------------------------------
+    // const loadObjModel = (materialURL, objectURL) => {
+    //   new MTLLoader().load(materialURL, materials => {
+    //     materials.preload();
+    //     //materials.Material.side = THREE.DoubleSide;
+    //     console.log("Loaded Materials");
+    //     var objLoader = new OBJLoader();
+    //     objLoader.setMaterials(materials);
+    //     objLoader.load(
+    //       objectURL,
+    //       object => {
+    //         //const root = object.detail.loaderRootNode;
+    //         console.log("Loaded Obj" + object);
+    //         let mesh = object;
+    //         object.position.z = -0.2;
+    //         object.scale.set(0.07, 0.07, 0.07);
+    //         scene.add(object);
+    //         mesh.position.set(0, 0, 0);
+    //         mesh.scale.set(0.07, 0.07, 0.07);
+    //       },
+    //       xhr => {
+    //         console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+    //       },
+    //       // called when loading has errors
+    //       error => {
+    //         console.log("An error happened" + error);
+    //       }
+    //     );
+    //   });
+    // };
+    // loadObjModel('../../indoor plant_02.mtl', '../../indoor plant_02.obj');
     // -------------------------------------------------------
     // var loader = new THREE.ObjectLoader();
+    // loader.load("01Alocasia_obj.obj",function ( obj ) {
+    //   scene.add( obj );
+    // });
+    // -------------------------------------------------------
+    // var loader = new THREE.OBJLoader();
     // loader.load(
     //   // resource URL
-    //   "flower.json",
+    //   'flower.json',
     //   // onLoad callback
     //   // Here the loaded data is assumed to be an object
     //   function ( obj ) {
     //     // Add the loaded object to the scene
+    //     console.log(obj);
     //     scene.add( obj );
     //   },
     //   // onProgress callback
@@ -58,7 +146,7 @@ export default function ARScreen() {
     //   },
     //   // onError callback
     //   function ( err ) {
-    //     console.error( 'An error happened' );
+    //     console.error( err );
     //   }
     // );
     // -------------------------------------------------------
@@ -69,13 +157,16 @@ export default function ARScreen() {
     //   scene.add(root);
     // });
     // -------------------------------------------------------
-    // var mtlLoader = new THREE.MaterialLoader();
-    // mtlLoader.load('/home/chryen/flourish/01Alocasia_obj.mtl', function(materials) {
+    // var mtlLoader = new MTLLoader();
+    // console.log("AHHHHHHHHHHHHHHHHHHHHHHH");
+    // mtlLoader.load('01Alocasia_obj.mtl', function(materials) {
+    //   console.log("NADASFADSFAS");
     //   materials.preload();
 
-    //   var objLoader = new THREE.ObjectLoader();
-    //   objLoader.load('/home/chryen/flourish/01Alocasia_obj.obj', function(object) {
+    //   var objLoader = new OBJLoader();
+    //   objLoader.load('01Alocasia_obj.obj', function(object) {
     //     object.position.z = -0.7;
+    //     console.log("this work idk?AGAsfdsadfadskjfaksdjfhdsjhfkjsdhfhksjdh");
     //     scene.add(object);
     //   });
     // });
@@ -111,27 +202,52 @@ export default function ARScreen() {
     cube.position.z = -0.4;
     // Add the cube to the scene
     scene.add(cube);
+
+    // loadModel();
   }
 
-    // When the phone rotates, or the view changes size, this method will be called.
-    const onResize = ({ x, y, scale, width, height }) => {
-    // Let's stop the function if we haven't setup our scene yet
-      if (!renderer) {
-        return;
-      }
-      camera.aspect = width / height;
-      camera.updateProjectionMatrix();
-      renderer.setPixelRatio(scale);
-      renderer.setSize(width, height);
-    };
+  // const loadModel = () => {
+  //   var loader = new THREE.ObjectLoader();
+  //   loader.load(
+  //     // resource URL
+  //     "flower.json",
+  //     // onLoad callback
+  //     // Here the loaded data is assumed to be an object
+  //     function ( obj ) {
+  //       // Add the loaded object to the scene
+  //       // obj.position.z = -0.2;
+  //       scene.add( obj );
+  //     },
+  //     // onProgress callback
+  //     function ( xhr ) {
+  //       console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
+  //     },
+  //     // onError callback
+  //     function ( err ) {
+  //       console.error( err );
+  //     }
+  //   );
+  // };
 
-  // Called every frame.
-    const onRender = () => {
-      // This will make the points get more rawDataPoints from Expo.AR
-      // points.update();
-      // Finally render the scene with the AR Camera
-      renderer.render(scene, camera);
-    };
+  // When the phone rotates, or the view changes size, this method will be called.
+  const onResize = ({ x, y, scale, width, height }) => {
+  // Let's stop the function if we haven't setup our scene yet
+    if (!renderer) {
+      return;
+    }
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+    renderer.setPixelRatio(scale);
+    renderer.setSize(width, height);
+  };
+
+    // Called every frame.
+  const onRender = () => {
+    // This will make the points get more rawDataPoints from Expo.AR
+    // points.update();
+    // Finally render the scene with the AR Camera
+    renderer.render(scene, camera);
+  };
 
   return (
     (<GraphicsView
