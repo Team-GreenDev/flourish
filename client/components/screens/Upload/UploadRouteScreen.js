147bd0f1043b0b7f21b345983f6407a72d5931b1
chryen <squirtlexomg@yahoo.com>
2020-05-25 15:43:34 -0500

    (feat) import ARScreen to upload route

--- client/components/screens/Upload/UploadRouteScreen.js
+++ client/components/screens/Upload/UploadRouteScreen.js
@@ -3,6 +3,7 @@ import { View } from 'react-native';
 import { NativeRouter, Switch, Route } from 'react-router-native';
 import UploadScreen from './UploadScreen';
 import CameraScreen from './CameraScreen';
+import ARScreen from '../../ARComponent/ARScreen';
 export default function UploadRouteScreen(){
 
   return (
@@ -11,6 +12,8 @@ export default function UploadRouteScreen(){
         <Switch>
           <Route exact path="/" component={UploadScreen} />
           <Route exact path="/camera" component={CameraScreen}/>
+          {/* <Route exact path="/ar" render={(props) => <ARScreen {...props} history={history} />}/> */}
+          <Route exact path="/ar" component={ARScreen}/>
         </Switch>
       </View>
     </NativeRouter>
