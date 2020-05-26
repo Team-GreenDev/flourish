16af38f034176bb30d4c39baa23f7a74a91a36cb
James Easter <james.m.easter@gmail.com>
2020-05-26 11:26:56 -0500

    (feat) add follow reducer to combineReducers

--- client/store/reducer.js
+++ client/store/reducer.js
@@ -5,15 +5,17 @@ import postsReducer from './slices/posts';
 import messagesReducer from './slices/messages';
 import authReducer from './slices/auth';
 import photoReducer from './slices/photo';
+import followReducer from './slices/follow';
 
 // Future reducers can be imported above like plantsReducer
 // then added as a key value to the combineReducers
 // combineReducers is then passed into the creation of the store in configureStore
 export default combineReducers({
-  plants: plantsReducer,
-  users: usersReducer,
-  posts: postsReducer,
-  messages: messagesReducer,
   auth: authReducer,
+  follow: followReducer,
+  messages: messagesReducer,
   photo: photoReducer,
+  plants: plantsReducer,
+  posts: postsReducer,
+  users: usersReducer,
 });
