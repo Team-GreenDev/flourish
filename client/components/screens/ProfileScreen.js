76eae0d14ceac658102ecd05df779c18c9e3fd39
James Easter <james.m.easter@gmail.com>
2020-05-27 15:15:45 -0500

    (feat) dynamically incorporate liked posts on profile page

--- client/components/screens/ProfileScreen.js
+++ client/components/screens/ProfileScreen.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react';
 import { StyleSheet, Text, View, Image, ScrollView, TouchableOpacity} from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { useDispatch, useSelector } from 'react-redux';
-import { loadPosts } from '../../store/slices/posts';
+import { loadPosts, loadLikedPosts } from '../../store/slices/posts';
 
   // MISSING FUNCTIONALITY: Dynamically render all posts current user has liked
 
@@ -12,50 +12,35 @@ export default function ProfileScreen() {
   const currentUser = useSelector(state => state.auth.currentUser);
   const followers = useSelector(state => state.follow.followers);
   const following = useSelector(state => state.follow.following);
+  const likedPosts = useSelector(state => state.posts.likedPosts);
+  const users = useSelector(state => state.users)
+
+  // Switch views between user's posts and liked posts
+  const [feedView, setFeedView] = useState(true);
+
+  const getUserById = (id) => users.list.filter((user) => user.id == id);
 
   useEffect(()=>{
     dispatch(loadPosts());
-  }, [])
+    dispatch(loadLikedPosts(currentUser.id));
+  }, [posts.postAdded])
 
   // Used to get the current users posts
   const getPostById = (id) => posts.list.filter((post) => post.user_id === id);
 
-  // Boolean to view current users posts or current users liked posts (could move to the store eventually)
-  const [boolean, setBoolean] = useState(true);
-
-  // Dummy data of 'liked' posts
-  const likeDummy = [
-    {
-      url: 'https://media.architecturaldigest.com/photos/5a94846e4692126e06f34f67/master/w_1600%2Cc_limit/popular-houseplants-pilea-peperomioides.jpg',
-      username: 'Rachel Davis',
-      message: 'Army of five, might even go pick up some more!',
-      tag: '#favplants #new2flourish',
-    },
-    {
-      url: 'https://secure.img1-fg.wfcdn.com/im/42349074/resize-h600%5Ecompr-r85/8506/85069027/Tejeda+5+Tier+Self-Watering+Wood+Vertical+Garden.jpg',
-      username: 'Jaime Vazquez',
-      message: 'Five tier wall garden is looking healthy this month',
-      tag: '#favplants #new2flourish',
-    },
-    {
-      url: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/high-angle-view-of-potted-cactus-royalty-free-image-1568039795.jpg?crop=0.752xw:1.00xh;0.139xw,0&resize=480:*',
-      username: 'Brenden Malone',
-      message: 'Check out this succulent collection',
-      tag: '#favplants #new2flourish',
-    },
-  ]
-
-  // Mapping over fake static posts that the user has 'liked'
-  const likeData = likeDummy.map(post => (
-    <View key={post.username}>
-      <View style={styles.post}>
-        <Image style={styles.image} source={{ uri: post.url }}/>
-        <Text style={styles.postUsername}>{post.username}</Text>
-        <Text style={styles.message}>{post.message}</Text>
-        <Text style={styles.tags}>{post.tag}</Text>
+  // Mapping liked posts
+  const likeData = likedPosts.slice().reverse().map(post => {
+    const user = getUserById(post.user_id)[0];
+    return (
+      <View key={post.id}>
+        <View style={styles.post}>
+          <Image style={styles.image} source={{ uri: post.url }}/>
+          <Text style={styles.postUsername}>{user.username}</Text>
+          <Text style={styles.message}>{post.text}</Text>
+          <Text style={styles.tags}>{post.tag}</Text>
+        </View>
       </View>
-    </View>
-  ));
+  )});
 
   // Maps over the current users posts to list them on profile feed
   const postData = getPostById(currentUser.id).reverse().map(post => (
@@ -79,19 +64,19 @@ export default function ProfileScreen() {
           <View style={styles.followCount}>
             <Text style={styles.followText}>{following.length} Following</Text>
             <Text style={styles.followText}>{followers.length} Followers</Text>
-            <Text style={styles.followText}>{currentUser.total_like} Seeds</Text>
+            <Text style={styles.followText}>{getUserById(currentUser.id)[0].total_like} Seeds</Text>
           </View>
         </TouchableOpacity>
           <View style={styles.iconView}>
             <TouchableOpacity>
-              <Ionicons style={styles.icon} name="md-grid" size={30} onPress={() => setBoolean(true)}/>
+              <Ionicons style={styles.icon} name="md-grid" size={30} onPress={() => setFeedView(true)}/>
             </TouchableOpacity>
             <TouchableOpacity>
-              <Ionicons style={styles.icon} name="md-thumbs-up" size={30} onPress={() => setBoolean(false)}/>
+              <Ionicons style={styles.icon} name="md-thumbs-up" size={30} onPress={() => setFeedView(false)}/>
             </TouchableOpacity>
           </View>
           <View>
-          {boolean ? (postData) : (likeData)}
+          {feedView ? (postData) : (likeData)}
           </View>
       </View>
     </ScrollView>
