5a61815e2e527127bc0df41a752b1f52267dce4c
Harley Padua <harleypadua@gmail.com>
2020-05-27 18:47:15 -0500

    (refactor) changed the names of what appears at the top of each route

--- client/components/navigation/BottomTabNavigator.js
+++ client/components/navigation/BottomTabNavigator.js
@@ -20,35 +20,35 @@ export default function BottomTabNavigator({ navigation, route }) {
   return (
     <BottomTab.Navigator initialRouteName={INITIAL_ROUTE_NAME}>
       <BottomTab.Screen
-        name="Home"
+        name=" "
         component={HomeRouteScreen}
         options={{
           tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-home" />,
         }}
       />
       <BottomTab.Screen
-        name="Search"
+        name="  "
         component={SearchRouteScreen}
         options={{
           tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-search" />,
         }}
       />
       <BottomTab.Screen
-        name="Camera"
+        name="   "
         component={UploadRouteScreen}
         options={{
           tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-add-circle" />,
         }}
       />
       <BottomTab.Screen
-        name="Messages"
+        name="     "
         component={MessageRouteScreen}
         options={{
           tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-text" />,
         }}
       />
       <BottomTab.Screen
-        name="Profile"
+        name="    "
         component={ProfileScreen}
         options={{
           tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-contact" />,
@@ -62,17 +62,17 @@ function getHeaderTitle(route) {
   const routeName = route.state?.routes[route.state.index]?.name ?? INITIAL_ROUTE_NAME;
 
   switch (routeName) {
-    case 'Home':
+    case ' ':
       return 'Flourish';
-    case 'Search':
-      return '';
-    case 'Camera':
-      return '';
+    case '  ':
+      return 'Search';
+    case '   ':
+      return 'Upload';
     case 'Notification':
       return 'What\'s happening!';
-    case 'Profile':
+    case '    ':
       return '';
-    case 'Messages':
-      return '';
+    case '     ':
+      return 'Messages';
   }
 }
